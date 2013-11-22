d04d1866ec31e0a6ace02f0ca5d55fcfdbf23d89
NeoAtlantis <rijndael@yeah.net>
2013-11-23 00:29:00 +0800

    doing a tree and service

--- geheimdienst.js
+++ geheimdienst.js
@@ -5,6 +5,7 @@
  * this system with access to global options.
  */
 require('./lib/baum.js');
+require('./lib/_.js');
 CONFIG = $.config.createConfig('./config/');
 
 var socketPath = CONFIG.get('socket-path');
@@ -13,11 +14,17 @@ var IPCServer = $.net.IPC.server(socketPath);
 console.log('IPC Server created at: ' + socketPath);
 
 IPCServer.on('data', function(e){
-    e.response.setHeader('content-type', 'text/html');
-    e.response.writeHead(200);
-    e.response.write('hello');
-    e.response.write($.nodejs.url.format(e.url));
-    e.end('Here is Regtable from Project Loesung.');
+    
+    _.symcrypt.encrypt('abcdefg', (new Date().getTime().toString()), function(err, cipherresult){
+        _.symcrypt.decrypt('abcdefg', cipherresult, function(err, data){
+            e.response.setHeader('content-type', 'text/html');
+            e.response.writeHead(200);
+            e.response.write(JSON.stringify(data.toString()) + '\n');
+            e.response.write($.nodejs.url.format(e.url));
+            e.end('Here is Regtable from Project Loesung.');
+        });
+    });
+
 });
 
 IPCServer.start();
--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 require('./lib/baum.js');
 CONFIG = $.config.createConfig('./config/');
-/*var socketPath = CONFIG.get('socket-path');
+var socketPath = CONFIG.get('socket-path');
 
 var IPCClient = $.net.IPC.client(socketPath);
+
+console.log(socketPath);
 
 IPCClient.request(
     '/abc/def',    
@@ -11,19 +13,24 @@ IPCClient.request(
             console.log(e.response.statusCode);
             console.log(e.response.headers);
             e.on('ready', function(data){
-                console.log(data);
+                console.log(data.raw);
             });
         };
     }
-);*/
+);
 
-var symcrypt = require('./lib/symcrypt')($);
+/*var symcrypt = require('./lib/symcrypt')($);
 var l = 1024;
 
 $.nodejs.async.waterfall(
     [
         function(callback){
-            callback(null, 'testthingsakkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk!');
+            source = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+            source += 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+            source += 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+            source += 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+            console.log(source.length);
+            callback(null, source);
             //$.nodejs.crypto.pseudoRandomBytes(l, callback);
         },
 
@@ -33,17 +40,19 @@ $.nodejs.async.waterfall(
 
         function(got, callback){
             console.log(got);
-            console.log(got.length);
+            console.log(got[0].length);
+            console.log(got[1].length);
 
             symcrypt.decrypt('abcdefg', got, callback);
         },
 
         function(got, callback){
             console.log(got.toString('ascii'));
+            console.log(got.length);
         },
 
     ],
 
     function(err, result){
     }
-);
+);*/
