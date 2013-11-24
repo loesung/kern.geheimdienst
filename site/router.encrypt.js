dc0a9a3043e4023a35fc80e68a50979518cca0f9
NeoAtlantis <rijndael@yeah.net>
2013-11-24 17:29:51 +0800

    required to be a buffer

--- site/router.encrypt.js
+++ site/router.encrypt.js
@@ -1,3 +1,35 @@
+/*
+ * API for encrypt given text.
+ *
+ * This uses the cascade encryption function(defined in /lib/symcrypt.js) to
+ * get a given plaintext encrypted.
+ *
+ * Request URI: /encrypt/<KEY-SOURCE>
+ *
+ * KEY-SOURCE:  choose between 'key' or 'codebook'. This will result in more
+ *              requirements in the request body.
+ *   - When using 'key', in the request body there MUST include following
+ *     items:
+ *       1) key,    HEX encoded key. The limit to key length does not exist.
+ *                  However, a key length of at least 256 bits is at least
+ *                  required by common practice, and up to 1024 bits is good
+ *                  in case of quantum computers.
+ *   - When using 'codebook', such items are required:
+ *       1)...
+ *
+ * In the request body, following items are required:
+ *   - plaintext,   HEX encoded plaintext, the plaintext that is going to be
+ *                  encrypted.
+ *
+ * Following items are optional in the request body. But they may cause errors
+ * when the resulting conditions are not satisfied.
+ *   - checksum,    HEX encoded, MD5 checksum of the ORIGINAL plaintext.
+ *                  Remember this is not the checksum of the HEX-encoded input
+ *                  and is designed to see, if what the program going to deal
+ *                  with is equal to what you want.
+ *      
+ */
+
 function job(e, matchResult, post, rueckruf){
     var keySource = matchResult[1],
         key = null,
@@ -5,7 +37,10 @@ function job(e, matchResult, post, rueckruf){
         checksum = post.parsed['checksum'];
     var workflow = [];
 
-    if(undefined == plaintext){
+    try{
+        plaintext = $.nodejs.buffer.Buffer(plaintext, 'hex');
+    } catch (e){
+        console.log(e);
         rueckruf(400);
         return;
     };
--- test.js
+++ test.js
@@ -18,7 +18,7 @@ IPCClient.request(
         };
     },
     {
-        post: 'checksum=&plaintext=' + encodeURIComponent('Test Text One\nText Text;'),
+        post: 'checksum=&plaintext=' + $.nodejs.buffer.Buffer('Test Text One\nText Text;').toString('hex'),
     }
 );
 
