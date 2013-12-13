da16b270d20aee3a5ebccb13462912c0edde9157
NeoAtlantis <rijndael@yeah.net>
2013-12-14 01:45:45 +0800

    test failed!

--- test.mss.js
+++ test.mss.js
@@ -2,11 +2,21 @@ require('./lib/baum.js');
 require('./lib/_.js');
 
 var mss = _.asymcrypt.signing('mss');
-/*var result = mss.sign(
+var result = mss.sign(
     new $.nodejs.buffer.Buffer('abcdefg'), 
     new $.nodejs.buffer.Buffer('abcdefg0')
-);*/
+);
+console.log(result);
 
+console.log(mss.root(
+    new $.nodejs.buffer.Buffer('abcdefg')
+));
+
+console.log(mss.verify(result[0], result[1], result[2],
+    new $.nodejs.buffer.Buffer('abcdefg0')
+));
+/*
 console.log(mss.derivePublicKey(
     new $.nodejs.buffer.Buffer('abcdefg0')
 ));
+*/
