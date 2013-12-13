0763fc661bb07018f4a6d864eae898ec8317f8ad
NeoAtlantis <rijndael@yeah.net>
2013-12-13 23:38:57 +0800

    more coding

--- test.mss.js
+++ test.mss.js
@@ -2,12 +2,11 @@ require('./lib/baum.js');
 require('./lib/_.js');
 
 var mss = _.asymcrypt.signing('mss');
-var tb = process.hrtime();
-for(var i = 1; i<= 10; i++){
-mss.sign(
+/*var result = mss.sign(
     new $.nodejs.buffer.Buffer('abcdefg'), 
     new $.nodejs.buffer.Buffer('abcdefg0')
-);
-};
-var te = process.hrtime();
-console.log('end', (te[1]-tb[1] + 1000000000*(te[0]-tb[0])) / 1000000000);
+);*/
+
+console.log(mss.derivePublicKey(
+    new $.nodejs.buffer.Buffer('abcdefg0')
+));
