70b94519730e2cd6f0e09c9d3f53318c02832ee0
NeoAtlantis <rijndael@yeah.net>
2013-12-22 20:43:41 +0800

    fix bug

--- test.storage.js
+++ test.storage.js
@@ -3,9 +3,8 @@ require('./lib/_.js');
 
 var storage = _.storage('./testStorage', 'abc');
 storage.load(function(){
-//  storage.table('table')('1', 'value = 1!');
-//    storage.table('table')('2', 'value = 2! second time');
-    console.log('I AM HERE.');
+//    storage.table('table')('1', 'value = 1!');
+//    storage.table('table')('2', {test: true, file:'abc'});
     console.log('*****************************', storage.table('table')('2'));
 //    storage.flush();
 });
