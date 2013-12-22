2c79618dd00f86a60f432e3077af2fd856c0d5a0
NeoAtlantis <rijndael@yeah.net>
2013-12-22 14:21:11 +0800

    save

--- test.storage.js
+++ test.storage.js
@@ -5,4 +5,6 @@ var storage = _.storage('./testStorage', 'abc');
 storage.load(function(){
     console.log('*****************************', storage.table('table')('1'));
     storage.table('table')('1', 'value1');
+    storage.table('table')('2', 'value2');
+//    storage.flush();
 });
