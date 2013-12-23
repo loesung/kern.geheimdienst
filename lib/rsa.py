ac6c7d0311d8f899f70b9c45d2aaa494cc74349d
NeoAtlantis <rijndael@yeah.net>
2013-12-23 16:23:01 +0800

    add policy

--- lib/rsa.py
+++ lib/rsa.py
@@ -17,15 +17,36 @@ The interface provides following features:
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_PSS
-from Crypto.Hash import SHA
+from Crypto.Hash import SHA, SHA224, SHA256, SHA384, SHA512
+
+_policy_ = {
+    2048: SHA256,
+    3072: SHA256,
+    4096: SHA384,
+    8192: SHA512,
+}
 
 def generate(bits):
-    if bits not in [2048, 3072, 4096, 8192]:
+    if bits not in _policy_.keys():
         return False
     key = RSA.generate(bits)
     pubKey = key.publickey().exportKey('DER')
     prvKey = key.exportKey('DER')
     return (pubKey, prvKey)
+
+def examine(key):
+    try:
+        key = RSA.importKey(key)
+        return {
+            'size': key.size(),
+            'private': key.has_private(),
+            'encrypt': key.can_encrypt() and (not key.has_private()),
+            'decrypt': key.can_encrypt() and key.has_private(),
+            'sign': key.can_sign() and key.has_private(),
+            'verify': key.can_sign() and (not key.has_private()),
+        }
+    except Exception,e:
+        return False
 
 def encrypt(pubKey, message):
     key = RSA.importKey(pubKey)
@@ -51,9 +72,15 @@ def decrypt(prvKey, ciphertext):
 
 def sign(prvKey, message):
     key = RSA.importKey(prvKey)
+    keySize = key.size() + 1
+    if keySize not in _policy_.keys():
+        return False
+    else:
+        hasher = _policy_[keySize]
+
     if not (key.has_private() and key.can_sign()):
         return False
-    h = SHA.new()
+    h = hasher.new()
     h.update(message)
     signer = PKCS1_PSS.new(key)
     signature = signer.sign(h)
@@ -61,9 +88,15 @@ def sign(prvKey, message):
 
 def verify(pubKey, message, signature):
     key = RSA.importKey(pubKey)
+    keySize = key.size() + 1
+    if keySize not in _policy_.keys():
+        return False
+    else:
+        hasher = _policy_[keySize]
+
     if key.has_private():
         return False
-    h = SHA.new()
+    h = hasher.new()
     h.update(message)
     verifier = PKCS1_PSS.new(key)
     if verifier.verify(h, signature):
@@ -91,7 +124,10 @@ SYNOPSIS
     except Exception,e:
         pass
 
-    pubKey, prvKey = generate(2048)
+    pubKey, prvKey = generate(3072)
 
     signature = sign(prvKey, 'message')
     print verify(pubKey, 'message', signature)
+
+    print examine(pubKey)
+    print examine(prvKey)
