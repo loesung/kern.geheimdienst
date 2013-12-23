4e4f673fbd80c2fe51937f4077b2bd71e362234b
NeoAtlantis <rijndael@yeah.net>
2013-12-24 02:34:33 +0800

    tested

--- test.rsa.js
+++ test.rsa.js
@@ -2,25 +2,38 @@ require('./lib/baum.js');
 require('./lib/_.js');
 
 var rsa0 = _.asymcrypt.encrypting('rsa'),
-    rsa1 = _.asymcrypt.signing('rsa')
+    rsa1 = _.asymcrypt.signing('rsa'),
+    rsa2 = _.asymcrypt.signing('rsa')
 ;
 
-var workflow = [
+var signature,
+    fake = new $.nodejs.buffer.Buffer('ab'),
+    tosign = new $.nodejs.buffer.Buffer('a');
 
+var workflow = [
     function(callback){
         rsa0.generate(2048, callback);
     },
-
     function(callback){
-        console.log(rsa0);
-        console.log(rsa1);
-
         rsa1.setPrivateKey(rsa0.getPrivateKey(), callback);
     },
+    function(callback){
+        rsa2.setPublicKey(rsa0.getPublicKey(), callback);
+    },
+
 
     function(callback){
-        console.log(rsa1);
+        rsa1.sign(tosign, function(err, result){
+            signature = result;
+            callback(null);
+        });
     },
+    function(s, callback){
+        rsa2.verify(fake, signature, function(err, result){
+            console.log(result);
+        });
+    },
+
 
 ];
 
