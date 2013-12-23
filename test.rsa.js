f7d437ae243d1f7166cf77bb30f7f3b6fb2ea64c
NeoAtlantis <rijndael@yeah.net>
2013-12-24 02:21:55 +0800

    testing

--- test.rsa.js
+++ test.rsa.js
@@ -1,14 +1,29 @@
 require('./lib/baum.js');
 require('./lib/_.js');
 
-var rsa = _.asymcrypt.encrypting('rsa');
-rsa.generate(2048, function(err, result){
-    if(null != err)
-        return console.log('err');
-   
-    rsa.encrypt(new $.nodejs.buffer.Buffer('a'), function(err, result){
-        rsa.decrypt(result, function(err, result){
-            console.log(err, result.toString());
-        });
-    });
+var rsa0 = _.asymcrypt.encrypting('rsa'),
+    rsa1 = _.asymcrypt.signing('rsa')
+;
+
+var workflow = [
+
+    function(callback){
+        rsa0.generate(2048, callback);
+    },
+
+    function(callback){
+        console.log(rsa0);
+        console.log(rsa1);
+
+        rsa1.setPrivateKey(rsa0.getPrivateKey(), callback);
+    },
+
+    function(callback){
+        console.log(rsa1);
+    },
+
+];
+
+$.nodejs.async.series(workflow, function(err){
+    console.log('ERROR', err);
 });
