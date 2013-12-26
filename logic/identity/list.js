0c0716f4a21f1b66c666fce06192317dbae8dd38
NeoAtlantis <rijndael@yeah.net>
2013-12-27 03:13:07 +0800

    save

--- logic/identity/list.js
+++ logic/identity/list.js
@@ -1,5 +1,13 @@
 module.exports = function(identity){
     return function(data, callback){
-        callback(null, identity());
+        var id = data.get.id;
+        if(undefined == id)
+            return callback(null, identity());
+
+        if(!_.object.test.identity.id(id)) return callback(400);
+       
+        var theIdentity = identity(id);
+        if(!theIdentity) return callback(404);
+        return callback(null, theIdentity);
     };
 };
