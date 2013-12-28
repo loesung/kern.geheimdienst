293f91fc9a09a642e9130746d59f8d72065c91e2
NeoAtlantis <rijndael@yeah.net>
2013-12-29 04:50:43 +0800

    problem with reloading database.

--- test/test.storage.js
+++ test/test.storage.js
@@ -1,8 +1,8 @@
 require('../lib/baum.js');
 require('../lib/_.js');
 
-var storage = _.storage('../testStorage', 'abc');
-storage.load(function(){
+var storage = _.storage('../testStorage');
+storage.load('abc', false, function(){
     storage.table('table')('1', 'value = 1!');
     storage.table('table')('2', {test: true, file:'abc'});
     console.log('*****************************', storage.table('table')('2'));
