6b936aebd30a07e7ed0204648e41217d09a195ad
NeoAtlantis <rijndael@yeah.net>
2013-12-29 19:01:57 +0800

    debug

--- test/storage.js
+++ test/storage.js
@@ -2,9 +2,9 @@ require('../lib/baum.js');
 require('../lib/_.js');
 
 var storage = _.storage('../testStorage');
-storage.load('abc', false, function(){
+storage.load('abc', true, function(){
     storage.table('table')('1', 'value = 1!');
     storage.table('table')('2', {test: true, file:'abc'});
     console.log('*****************************', storage.table('table')('2'));
-    storage.flush();
+//    storage.flush();
 });
