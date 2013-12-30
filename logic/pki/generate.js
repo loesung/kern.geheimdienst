7f03aa9ef12b0c83b2b25b9da383890f0794f81b
NeoAtlantis <rijndael@yeah.net>
2013-12-31 03:31:43 +0800

    doing

--- logic/pki/generate.js
+++ logic/pki/generate.js
@@ -1,3 +1,13 @@
+function RSASignOnly(bits, dataSource, identityID){
+    return function(callback){
+    };
+};
+
+function MSS224SignOnly(dataSource, identityID){
+    return function(callback){
+    };
+};
+
 module.exports = function(dataSource){
     return function(data, callback){
         var enumerator = _.object.enumerate.pki;
@@ -12,17 +22,24 @@ module.exports = function(dataSource){
         ){
             return callback(400, {
                 keyAlgo: Object.keys(enumerator.keyAlgo),
-
             });
         };
 
         switch(keyAlgo){
             case 'RSA2048SignOnly':
-                
-                break;
+                return RSASignOnly(2048, dataSource, identityID)(callback);
+            case 'RSA3072SignOnly':
+                return RSASignOnly(3072, dataSource, identityID)(callback);
+            case 'RSA4096SignOnly':
+                return RSASignOnly(4096, dataSource, identityID)(callback);
+            case 'RSA8192SignOnly':
+                return RSASignOnly(8192, dataSource, identityID)(callback);
+            case 'MSS224SignOnly':
+                return MSS224SignOnly(dataSource, identityID)(callback);
             default: 
                 return callback(400);
         };
-        
+
+        return callback(501); // leakage
     };
 };
