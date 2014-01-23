b9dca2840af7253a2aeb863cfab34d24bd45eed3
NeoAtlantis <rijndael@yeah.net>
2014-01-23 16:38:20 +0800

    save

--- test/otr.js
+++ test/otr.js
@@ -4,4 +4,8 @@ require('../lib/_.js');
 var sharedsecret = new $.nodejs.buffer.Buffer('the secret key', 'ascii');
 var otrAlice = _.otr(sharedsecret), otrBob = _.otr(sharedsecret);
 
-console.log(otrAlice);
+otrAlice.on('send', function(message){
+    console.log('alice should send following ciphertext: \n', message);
+});
+
+otrAlice.send(new $.nodejs.buffer.Buffer('hallo', 'ascii'));
