75c4b3e47f65b76f4e639b1d8d374b0538da0094
NeoAtlantis <rijndael@yeah.net>
2014-01-23 18:06:33 +0800

    save otr. test passed

--- test/otr.js
+++ test/otr.js
@@ -6,6 +6,15 @@ var otrAlice = _.otr(sharedsecret), otrBob = _.otr(sharedsecret);
 
 otrAlice.on('send', function(message){
     console.log('alice should send following ciphertext: \n', message);
+    otrBob.receive(message);
+});
+
+otrBob.on('receive', function(message){
+    console.log('bob received following text: \n', message);
+});
+
+otrBob.on('error', function(err){
+    console.log('bob found an error: \n', err);
 });
 
 otrAlice.send(new $.nodejs.buffer.Buffer('hallo', 'ascii'));
