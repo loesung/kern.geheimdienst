aa8ee7e3036a037f66e58a514ba3beefad48c98e
NeoAtlantis <rijndael@yeah.net>
2014-01-25 20:43:42 +0800

    save

--- core/decrypt/examine.js
+++ core/decrypt/examine.js
@@ -1,5 +1,10 @@
 module.exports = function(storage){
-    // examine a ciphertext
+    /* examine a ciphertext
+     *
+     * the idea is to find out, what a form the user is expected to give.
+     * shall a private key in database doesn't exist, a failure should also
+     * be given out.
+     */
     return function(message, callback){
         var overview = _.package.parse(message);
         if(null == overview) return callback(Error('unrecognized-data'));
