5bdcd13dff7209c9a4dfb8f9acc736e0bfe0dfb9
NeoAtlantis <rijndael@yeah.net>
2014-01-29 22:33:30 +0800

    save

--- core/identity/list.js
+++ core/identity/list.js
@@ -1,16 +1,32 @@
 module.exports = function(storage, hints, callback){
     if(!$.types.isArray(hints)) hints = [hints,];
 
-    var search = [];
+    var searchKey = [], searchSubject = [], defaultEncoding;
 
     for(var i in hints){
         var hint = hints[i];
-        if($.types.isString(hint))
-            hint = new $.nodejs.buffer.Buffer(hint);
-        else if(!$.types.isBuffer(hint))
-            continue;
-        search.push(hint);
+        if($.types.isBuffer(hint)) hint = hint.toString('ascii');
+        if($.types.isString(hint)){
+            if(!/^[0-9a-z_\(\)\[\]\.]+$/i.test(hint)) continue;
+            if(
+                /^[0-9a-f]+$/i.test(hint) &&
+                hint.length % 2 == 0
+            )
+                searchKey.push(hint.toLowerCase());
+            searchSubject.push(hint);
+        };
     };
 
-    var allIdentites
+    var match = [];
+   
+    var allIdentities = storage.table('identity')(), cachedIdentities = {};
+    for(var key in allIdentities){
+        try{
+            var identity = _.package.parse(allIdentities[key]);
+            if(identity[0] != 'identity') continue;
+            cachedIdentities[key] = identity[1].content.subject.toString();
+        } catch(e){
+            continue;
+        };
+    };
 };
