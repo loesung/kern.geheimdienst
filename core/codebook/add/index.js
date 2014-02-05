36d1e87ead56c82d7874b066a4883e005b29a9d8
NeoAtlantis <rijndael@yeah.net>
2014-02-05 09:25:13 +0800

    doing

--- core/codebook/add/index.js
+++ core/codebook/add/index.js
@@ -1,7 +1,14 @@
+/*
+ * GLOBAL SETTINGS FOR DEFAULT CODEBOOK ADDITION
+ */
+
+// Default codebook life(seconds). Default 2592000 equals to 30 days.
+DEFAULT_CODEBOOK_LIFE = 2592000;
+
 function add(storage){
     var self = this;
 
-    this.add = require('./sharedsecret.js')(storage);
+    this.sharedsecret = require('./sharedsecret.js')(storage);
 
     return this;
 };
--- core/codebook/add/sharedsecret.js
+++ core/codebook/add/sharedsecret.js
@@ -1,10 +1,26 @@
 module.exports = function(storage){
-    return function(memberIDs, sharedsecret, options){
+    return function(memberIDs, sharedsecret, options, callback){
         
         // regulate member IDs
-        
+        var members = [];
+        for(var i in memberIDs){
+            var memberID = memberIDs[i];
+            if($.types.isBuffer(memberID))
+                memberID = memberID.toString('hex');
+            if($.types.isString(memberID)){
+                memberID = memberID.toLowerCase();
+                if(!/^[0-9a-f]$/.test(memberID)) continue;
+                if(!storage.table('identity')(memberID))
+                    return callback(Error('member-not-recognized'));
+                members.push(memberID);
+            };
+        };
+
+        members.sort();
 
         // set validate date range
+        var validAfter = Math.round(new Date().getTime() / 1000);
+        var validTo = validAfter + DEFAULT_CODEBOOK_LIFE; 
         
         // derive credential
         
