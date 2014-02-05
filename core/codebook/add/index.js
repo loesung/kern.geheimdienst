3e0256b1d9c3b071126d6164d15ce823a7f20d5a
NeoAtlantis <rijndael@yeah.net>
2014-02-05 10:45:28 +0800

    save

--- core/codebook/add/index.js
+++ core/codebook/add/index.js
@@ -5,6 +5,12 @@
 // Default codebook life(seconds). Default 2592000 equals to 30 days.
 DEFAULT_CODEBOOK_LIFE = 2592000;
 
+// Max. codebook life(seconds). Default 315360000 equals to 10 years.
+CODEBOOK_LIFE_MAX = 315360000;
+
+// Min. codebook life(seconds). Default 3600 equals to 1 hour. 
+CODEBOOK_LIFE_MIN = 3600;
+
 function add(storage){
     var self = this;
 
--- core/codebook/add/sharedsecret.js
+++ core/codebook/add/sharedsecret.js
@@ -1,3 +1,6 @@
+// Min. length of a sharedsecret.
+MIN_SHAREDSECRET_LENGTH = 32;
+
 module.exports = function(storage){
     return function(memberIDs, sharedsecret, options, callback){
         
@@ -15,14 +18,58 @@ module.exports = function(storage){
                 members.push(memberID);
             };
         };
-
         members.sort();
+        for(var i in members)
+            members[i] = new $.nodejs.buffer.Buffer(members[i], 'hex');
+
 
         // set validate date range
         var validAfter = Math.round(new Date().getTime() / 1000);
         var validTo = validAfter + DEFAULT_CODEBOOK_LIFE; 
+
+        if($.types.isNumber(options.validAfter))
+            validAfter = options.validAfter;
+
+        if($.types.isNumber(options.validTo))
+            validTo = options.validTo;
+
+        if(!(
+            validTo - validAfter >= CODEBOOK_LIFE_MIN &&
+            validTo - validAfter <= CODEBOOK_LIFE_MAX &&
+            validAfter > 0
+        ))
+            return callback(Error('invalid-codebook-lifecycle'));
+
+
+        // check `sharedsecret`
+        if(!$.types.isBuffer(sharedsecret))
+            return callback(Error('sharedsecret-not-buffer'));
         
+
+        var workflow = [];
+
         // derive credential
+        workflow.push(function(callback){
+            $.nodejs.crypto.pbkdf2(
+                sharedsecret,
+                'CODEBOOK-DERIVED-BY-SHAREDSECRET', // cannot be negotiated.
+                1024,
+                1024,
+                callback
+            );
+        });
+
+        // generate entry
+        workflow.push(function(derivedKey, callback){
+            var id = _.digest.whirlpool(derivedKey);
+            var entry = {
+                members: members,
+                credential: derivedKey,
+                validAfter: validAfter,
+                validTo: validTo,
+                id: id,
+            };
+        });
         
         // save to storage
         
